216b0f2155e926c7569efb25137fb3283a7d3d42
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-07 07:09:58 +0900

    Update Power World.lean

--- Power World.lean
+++ Power World.lean
@@ -42,3 +42,26 @@ nth_rewrite 3 [mul_comm]
 nth_rewrite 5 [mul_comm]
 repeat rw [← mul_assoc]
 rfl
+
+pow_pow
+induction m with d hd
+rw [zero_mul]
+repeat rw [pow_zero]
+rw [one_pow]
+rfl
+rw [pow_succ, mul_pow, hd, succ_mul, pow_add]
+rfl
+
+add_sq
+repeat rw [pow_two]
+rw [add_mul]
+repeat rw [mul_add]
+repeat rw [add_assoc]
+nth_rewrite 2 [← add_assoc]
+nth_rewrite 3 [mul_comm]
+rw [← two_mul]
+nth_rewrite 2 [add_comm]
+rw [← mul_assoc]
+rfl
+
+FLT
