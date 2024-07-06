2f71cc9b1788e225b52987e133a97db99fa2e367
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-07 05:24:44 +0900

    Update Multiplication World.lean

--- Multiplication World.lean
+++ Multiplication World.lean
@@ -34,3 +34,22 @@ rfl
 two_mul
 rw [two_eq_succ_one, succ_mul, one_mul]
 rfl
+
+mul_add
+induction c with d hd
+rw [add_zero, mul_zero, add_zero]
+rfl
+rw [add_succ, mul_succ, hd, mul_succ, add_assoc]
+rfl
+
+add_mul
+rw [mul_comm, mul_add]
+repeat rw [mul_comm c]
+rfl
+
+mul_assoc
+induction c with d hd
+repeat rw [mul_zero]
+rfl
+rw [mul_succ, hd, mul_succ, ← mul_add]
+rfl
