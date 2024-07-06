b2f3204ec921c58f73757c16f7c9281cd11554e3
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-07 08:08:40 +0900

    Update Advanced Addition World.lean

--- Advanced Addition World.lean
+++ Advanced Addition World.lean
@@ -12,3 +12,26 @@ add_left_cancel
 repeat rw [add_comm n]
 exact add_right_cancel a b n
 
+add_left_eq_self
+nth_rewrite 2 [← zero_add y]
+exact add_right_cancel x 0 y
+
+add_right_eq_self
+rw [add_comm]
+exact add_left_eq_self y x
+
++'cases' tactics
+add_right_eq_zero
+cases b with d
+rw [add_zero]
+intro h
+exact h
+intro h
+symm at h
+rw [add_succ] at h
+apply zero_ne_succ at h
+cases h
+
+add_left_eq_zero
+rw [add_comm]
+exact add_right_eq_zero b a
