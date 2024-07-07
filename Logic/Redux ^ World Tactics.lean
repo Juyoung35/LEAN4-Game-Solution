6ec64f5328d3dcaa02e9f19963ecb414c9f58477
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-07 14:12:15 +0900

    Create Redux ^ World Tactics.lean

---
+++ Logic/Redux ^ World Tactics.lean
@@ -0,0 +1,43 @@
+assumption
+
+constructor
+assumption
+assumption
+
+constructor
+constructor
+assumption
+assumption
+constructor
+assumption
+assumption
+
+cases h
+assumption
+
+cases h
+assumption
+
+cases h1
+cases h2
+constructor
+assumption
+assumption
+
+cases h
+cases left
+cases right_1
+cases left
+cases left_2
+assumption
+
+cases h
+cases right
+cases right_1
+cases left_2
+cases left
+constructor
+assumption
+constructor
+assumption
+assumption
