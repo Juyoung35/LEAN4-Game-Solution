2f540692cedf1558e0fa50b2025779d6525e02ee
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-08 14:58:23 +0900

    Update Family Intersection World.lean

--- STG/Family Intersection World.lean
+++ STG/Family Intersection World.lean
@@ -11,3 +11,21 @@ have h4 := h1 h3
 exact h2 y h4
 
 +mem_pair
+ext x
+apply Iff.intro
+intro h1 y h2
+rewrite [mem_pair] at h2
+rewrite [mem_inter_iff] at h1
+cases' h2 with ha hb
+rw [ha]
+exact h1.left
+rw [hb]
+exact h1.right
+intro h1
+rewrite [mem_inter_iff]
+rewrite [mem_sInter] at h1
+apply And.intro
+have ha : A ⊆ A := Subset.refl A
+have haa : A = A := Subset.antisymm ha ha
+have hab : A = A ∨ A = B := Or.inl haa
+rewrite [← mem_pair A A B] at hab
