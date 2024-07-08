274ad36bf9f4bfad29dd1f6e15f44fa58672987f
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-08 09:41:48 +0900

    Update Intersection World.lean

--- STG/Intersection World.lean
+++ STG/Intersection World.lean
@@ -16,3 +16,32 @@ intro x
 intro h
 rewrite [mem_inter_iff]
 exact And.intro (h1 h) (h2 h)
+
+inter_subset_swap
+intro x h
+rewrite [mem_inter_iff] at h
+have h2 := And.intro h.right h.left
+rewrite [← mem_inter_iff x B A] at h2
+exact h2
+
+inter_comm
+apply Subset.antisymm
+exact inter_subset_swap A B
+exact inter_subset_swap B A
+
++"ext" tactics
+inter_assoc
+ext x
+apply Iff.intro
+intro h1
+rewrite [mem_inter_iff] at h1
+have h2 := h1.left
+have h3 := h1.right
+rewrite [mem_inter_iff] at h2
+exact And.intro h2.left (And.intro h2.right h3)
+intro h1
+rewrite [mem_inter_iff] at h1
+have h2 := h1.left
+have h3 := h1.right
+rewrite [mem_inter_iff] at h3
+exact And.intro (And.intro h2 h3.left) h3.right
