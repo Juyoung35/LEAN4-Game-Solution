8737f87464687c069a33fd48a89ec749edc314fd
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-09 19:09:40 +0900

    Update NotIntro.lean

--- Logic/NotIntro.lean
+++ Logic/NotIntro.lean
@@ -12,3 +12,17 @@ modus_tollens
 exact h1 ≫ h2
 
 exact λ(a: A) ↦ h a a
+
+exact λ(c: C) ↦ h (λ(_: B) ↦ c)
+
+exact λ(p: ¬S ∧ C) ↦ p.left s
+
+exact λ(p: P ∧ A) ↦ (h p.left) p.right
+
+exact λ(p: P) ↦ λ(a: A) ↦ h (and_intro p a)
+// exact λp a ↦ h ⟨p, a⟩
+
+not_not_not
+exact λ(a: A) ↦ h λ(na: ¬A) ↦ na a
+
+exact λ(b: ¬B) ↦ h (λ(bb: B) ↦ false_elim (b bb))
