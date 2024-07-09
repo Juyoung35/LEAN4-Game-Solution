78d5b199e36677ded3dbc581eff9aeac90e3f2ac
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-09 15:29:05 +0900

    Update OrIntro.lean

--- Logic/OrIntro.lean
+++ Logic/OrIntro.lean
@@ -12,3 +12,14 @@ exact or_elim h or_inr or_inl
 
 exact or_elim h2 (h1 ≫ or_inl) or_inr // 답지 베낌
 
+exact and_intro (or_elim h or_inl λ(q: H ∧ U) ↦ (or_inr q.left)) (or_elim h or_inl λ(q: H ∧ U) ↦ (or_inr q.right))
+
+exact or_elim h.right (λ(p: H) ↦ or_inl ⟨h.left, p⟩) (λ(q: U) ↦ or_inr ⟨h.left, q⟩)
+/*
+have g := h.left
+exact or_elim h.right
+  (and_intro g ≫ or_inl)
+  (and_intro g ≫ or_inr)
+*/
+
+exact λ(p: K ∨ I) ↦ or_elim p (h ≫ or_inr) or_inl
