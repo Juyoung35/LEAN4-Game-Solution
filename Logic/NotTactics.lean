6697be1b07248eae8e8b53da4bea8e054fe6428d
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-10 08:45:41 +0900

    Update NotTactics.lean

--- Logic/NotTactics.lean
+++ Logic/NotTactics.lean
@@ -5,3 +5,44 @@ assumption
 intro h
 contradiction
 
++"exfalso" tactics
+intro h
+apply h at h'₁
+assumption
+
+intro h
+apply h.right h.left
+
+intro h
+apply h2 (h1 h)
+
++modus_tollens
+intro h2
+apply h h2
+assumption
+
+intro
+apply h
+intro
+assumption
+
+intro
+apply a.left h
+
+intro
+apply h a.left a.right
+
+intro p q
+apply h ⟨p, q⟩
+
+intro
+apply h
+intro
+apply a_1
+assumption
+
+intro
+apply h
+intro
+contradiction
+
