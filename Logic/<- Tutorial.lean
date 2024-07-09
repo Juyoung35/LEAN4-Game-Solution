7f0aa030a2fca37fe6e5aac030a005b36afda175
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-09 10:27:57 +0900

    Update <- Tutorial.lean

--- Logic/<- Tutorial.lean
+++ Logic/<- Tutorial.lean
@@ -26,4 +26,13 @@ exact fun p => h2 (h1 p)
 
 exact (h1 ≫ h3 ≫ h5) p
 
-and_imp
+// and_imp
+exact λ(p : C) ↦ λ(q : D) ↦ h (and_intro p q)
+// exact λ(p : C)(q : D) ↦ h (and_intro p q)
+// exact λp q ↦ h (and_intro p q)
+
+// and_imp 2
+exact λ(p: C ∧ D) ↦ h p.left p.right
+
+// → distributes over ∧
+exact λ(p: S) ↦ and_intro (h.left p) (h.right p)
