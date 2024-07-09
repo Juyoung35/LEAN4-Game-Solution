be55a9d60cbaae1f1ecb050bea191adf412fe293
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-09 20:13:21 +0900

    Update IifIntro.lean

--- Logic/IifIntro.lean
+++ Logic/IifIntro.lean
@@ -4,3 +4,17 @@ exact iff_intro hsj hjs
 +iff_mp
 +iff_mpr
 exact and_intro (λ(b: B) ↦ iff_mp h b) (λ(p: ¬P) ↦ iff_mpr h p)
+
+exact λ(p: P) ↦ iff_mp h1 (h2 p)
+
+exact λ(r: R) ↦ h2 (iff_mpr h1 r)
+
++"rw" tactics
+rw [← h1] at h2
+exact h2
+
++and_assoc
++or_assoc
+rw [← or_assoc, ← and_assoc] at h
+exact h
+// exact or_assoc.mp ≫ h ≫ imp_trans and_assoc.mp
