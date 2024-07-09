ae50baa7306c425e9e6e9c2b2dedf68144b66d8e
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-07-10 07:55:40 +0900

    Create OrTactics.lean

---
+++ Logic/OrTactics.lean
@@ -0,0 +1,53 @@
++"left" tactics
+left
+assumption
+
++"right" tactics
+right
+assumption
+
+apply or_elim h3 h1 h2
+
+or_comm
+cases h
+right
+assumption
+left
+assumption
+
+cases h2
+left
+apply h1 h
+right
+assumption
+
+cases h
+constructor
+left
+assumption
+left
+assumption
+cases h_1
+constructor
+right
+assumption
+right
+assumption
+
+cases h
+cases right
+left
+constructor
+assumption
+assumption
+right
+constructor
+assumption
+assumption
+
+intro h1
+cases h1
+right
+apply h h_1
+left
+assumption
